937dcee900ecd68f0f9dbd3f24b6eaf12356eb78
Diego Araneda <daranedag@gmail.com>
2025-10-04 09:21:33 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diegodev/', // Reemplaza 'diegodev' con el nombre exacto de tu repositorio
+  base: '/', // Reemplaza 'diegodev' con el nombre exacto de tu repositorio
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
