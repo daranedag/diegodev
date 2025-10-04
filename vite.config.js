ff121259a8c3034e5981bfc84c1602d38476dd0c
Diego Araneda <daranedag@gmail.com>
2025-10-04 09:15:23 -0300

    Configure github pages CI/CD

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/diegodev/', // Reemplaza 'diegodev' con el nombre exacto de tu repositorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
