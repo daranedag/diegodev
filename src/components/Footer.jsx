926c6717c1322c66c1272bdfe7376bea82db022c
Diego Araneda <daranedag@gmail.com>
2026-04-12 17:45:30 -0400

    Año del footer modificado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <p className="text-center text-sm text-gray-600 dark:text-gray-400">
-                    {t('footer.copyright')}
+                    {t('footer.copyright', { year: new Date().getFullYear() })}
                 </p>
             </div>
         </footer>
